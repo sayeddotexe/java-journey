ac872595561624d33876c60608e6c875fe852a64
Sayed Ismail Ahmed <contact.ismailahmed@gmail.com>
2025-07-15 10:48:47 +0100

    feat: add basic Hello World program

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,14 @@
+// This is the main class of the program. In Java, every application must contain a class.
+public class Main {
+
+    // This is the main method. It is the entry point of any Java application.
+    // The 'String[] args' parameter allows the program to accept command-line arguments.
+    public static void main(String[] args) {
+
+        // Declare and initialize a String (rerefence type) variable named 'helloWorld' with the value "Hello World".
+        String helloWorld = "Hello World"; // This variable holds the message to be printed.
+
+        // Print the value of the 'helloWorld' variable to the console.
+        System.out.println(helloWorld);
+    }
+}
